87547942bc062f931db38cba6a9a17df0475bf8f
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 15:21:35 -0300

    Criação do model Video

---
+++ VideoApi/Model/Video.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+	public class Video
+	{
+		[Required]
+		public int Id { get; private set; }
+
+		[Required(ErrorMessage = "O campo título é obrigatório")]
+		[StringLength(128, ErrorMessage = "O campo título não pode ultrapassar 128 caracteres")]
+		public string Titulo { get; private set; }
+
+		[Required(ErrorMessage = "O campo descrição é obrigatório")]
+		[StringLength(255, ErrorMessage = "O campo título não pode ultrapassar 255 caracteres")]
+		public string Descricao { get; private set; }
+
+		[Required(ErrorMessage = "O campo url é obrigatório")]
+		[StringLength(128, ErrorMessage = "O campo título não pode ultrapassar 128 caracteres")]
+		public string Url { get; private set; }
+
+		public Video(int id, string titulo, string descricao, string url)
+		{
+			Id = id;
+			Titulo = titulo;
+			Descricao = descricao;
+			Url = url;
+		}
+	}
+}
