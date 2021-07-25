35b779671276fbbde2b316b24a9610ea1897b860
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 16:32:05 -0300

    Criação da classe VideoService e método para retornar todos os videos

---
+++ VideoApi/Services/VideoService.cs
@@ -0,0 +1,24 @@
+﻿using Dao;
+using Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+	public class VideoService
+	{
+		private readonly VideoDao _videoDao;
+
+		public VideoService(VideoDao videoDao)
+		{
+			_videoDao = videoDao;
+		}
+
+		public List<Video> GetAllVideos()
+		{
+			List<Video> videoList = _videoDao.GetAllVideosIQueryable().ToList();
+
+			return videoList;
+		}
+	}
+}
