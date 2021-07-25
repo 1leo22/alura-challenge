7f832ea91c8c9198c70b1754e2fe351ef11b4bb6
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 16:55:18 -0300

    Criação da classe VideoController

---
+++ VideoApi/VideoApi/Controllers/VideoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+using System.Collections.Generic;
+
+namespace VideoApi.Controllers
+{
+	[ApiController]
+	[Route("api/videos")]
+	public class VideoController : ControllerBase
+	{
+		private readonly VideoService _videoService;
+
+		public VideoController(VideoService videoService)
+		{
+			_videoService = videoService;
+		}
+
+		[HttpGet]
+		public IActionResult GetAllVideos()
+		{
+			List<Video> videoList = _videoService.GetAllVideos();
+			return Ok(videoList);
+		}
+	}
+}
