038102d9c615647ee2aa47a1c5a3c2cdc2a594ea
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 17:09:55 -0300

    Tornando o set da classe Video público

--- VideoApi/Model/Video.cs
+++ VideoApi/Model/Video.cs
@@ -6,19 +6,19 @@ namespace Model
 	{
 		[Key]
 		[Required]
-		public int Id { get; private set; }
+		public int Id { get; set; }
 
 		[Required(ErrorMessage = "O campo título é obrigatório")]
 		[StringLength(128, ErrorMessage = "O campo título não pode ultrapassar 128 caracteres")]
-		public string Titulo { get; private set; }
+		public string Titulo { get; set; }
 
 		[Required(ErrorMessage = "O campo descrição é obrigatório")]
 		[StringLength(255, ErrorMessage = "O campo título não pode ultrapassar 255 caracteres")]
-		public string Descricao { get; private set; }
+		public string Descricao { get; set; }
 
 		[Required(ErrorMessage = "O campo url é obrigatório")]
 		[StringLength(128, ErrorMessage = "O campo título não pode ultrapassar 128 caracteres")]
-		public string Url { get; private set; }
+		public string Url { get; set; }
 
 		public Video(int id, string titulo, string descricao, string url)
 		{
