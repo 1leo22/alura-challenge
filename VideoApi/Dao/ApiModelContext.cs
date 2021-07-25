4bcfc83652681f17db27e0cb477154881e55f585
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 15:44:47 -0300

    Configuração do EntityFramework

---
+++ VideoApi/Dao/ApiModelContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Dao
+{
+	public class ApiModelContext : DbContext
+	{
+		DbSet<Video> Videos { get; set; }
+
+		public ApiModelContext(DbContextOptions<ApiModelContext> options) : base(options)
+		{
+		}
+	}
+}
