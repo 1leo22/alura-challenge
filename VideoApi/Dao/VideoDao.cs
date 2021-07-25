6e361c95ca791ad11dfa30b4a402a452da3e72b9
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-07-25 16:30:41 -0300

    Criação da classe VideoDao e metodo para retornar todos os videos

---
+++ VideoApi/Dao/VideoDao.cs
@@ -0,0 +1,21 @@
+﻿using Dao.Context;
+using Model;
+using System.Linq;
+
+namespace Dao
+{
+	public class VideoDao
+	{
+		private readonly ApiModelContext _context;
+
+		public VideoDao(ApiModelContext context)
+		{
+			_context = context;
+		}
+
+		public IQueryable<Video> GetAllVideosIQueryable()
+		{
+			return _context.Videos;
+		}
+	}
+}
