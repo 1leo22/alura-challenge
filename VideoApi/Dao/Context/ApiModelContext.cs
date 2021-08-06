0d2e04602995a610010b384f5318c741aaf1b002
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-08-05 22:38:55 -0300

    Criando metodo OnModelCreating para adicionar valor inicial a tabela categoria

--- VideoApi/Dao/Context/ApiModelContext.cs
+++ VideoApi/Dao/Context/ApiModelContext.cs
@@ -13,5 +13,18 @@ namespace Dao.Context
 		public DbSet<Video> Videos { get; set; }
 
 		public DbSet<Categoria> Categorias { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Categoria>()
+				.HasData(
+					new Categoria
+					{
+						Id = 1,
+						Titulo = "Livre",
+						Cor = "Verde"
+					}
+				);
+		}
 	}
 }
---
+++ VideoApi/Dao/Migrations/20210806013512_Adicionando dado inicial na tabela categoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dao.Migrations
+{
+    public partial class Adicionandodadoinicialnatabelacategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categoria",
+                columns: new[] { "id", "cor", "titulo" },
+                values: new object[] { 1, "Verde", "Livre" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categoria",
+                keyColumn: "id",
+                keyValue: 1);
+        }
+    }
+}
