39dab59a76fbf56e1fd3c76866667b49a1afdefd
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-08-05 22:42:44 -0300

    Criando classe CategoriaController

---
+++ VideoApi/VideoApi/Controllers/CategoriaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoApi.Controllers
+{
+
+	[ApiController]
+	[Route("api/categorias")]
+	public class CategoriaController : ControllerBase
+	{
+		public CategoriaController()
+		{
+
+		}
+	}
+}
