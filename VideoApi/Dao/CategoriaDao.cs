f9379d285ba3a145fa48ad9949673f1efdfee302
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-08-07 15:01:19 -0300

    Criada classe CategoriaDao

---
+++ VideoApi/Dao/CategoriaDao.cs
@@ -0,0 +1,13 @@
+﻿using Dao.Context;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Dao
+{
+	public class CategoriaDao : AbstractDao<Categoria>
+	{
+		public CategoriaDao(DbSet<Categoria> context, ApiModelContext modelContext) : base(context, modelContext)
+		{
+		}
+	}
+}
