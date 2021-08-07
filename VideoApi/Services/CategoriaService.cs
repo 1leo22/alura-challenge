8cd15afee0264e4359e96ef3b15128dfc64224eb
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-08-07 15:12:02 -0300

    Criada classe CategoriaService

---
+++ VideoApi/Services/CategoriaService.cs
@@ -0,0 +1,47 @@
+﻿using Dao.Interfaces;
+using Model;
+using Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+	public class CategoriaService : ICategoriaService
+	{
+		private readonly IAbstractDao<Categoria> _categoriaDao;
+
+		public CategoriaService(IAbstractDao<Categoria> categoriaDao)
+		{
+			_categoriaDao = categoriaDao;
+		}
+
+		public List<Categoria> GetAllCategorias()
+		{
+			List<Categoria> videoList = _categoriaDao.GetAll().ToList();
+
+			return videoList;
+		}
+
+		public Categoria GetCategoriaById(int id)
+		{
+			Categoria categoria = _categoriaDao.Get(id);
+
+			return categoria;
+		}
+
+		public void AddCategoria(Categoria categoria)
+		{
+			_categoriaDao.Add(categoria);
+		}
+
+		public void DeleteCategoria(Categoria categoria)
+		{
+			_categoriaDao.Remove(categoria);
+		}
+
+		public void UpdateCategoria(Categoria categoria)
+		{
+			_categoriaDao.Update(categoria);
+		}
+	}
+}
