111b43911307d5d54235bd03b2df2b97846d4e12
Leonardo <44236679+1leo22@users.noreply.github.com>
2021-08-07 15:13:38 -0300

    Criada interface ICategoriaService

---
+++ VideoApi/Services/Interfaces/ICategoriaService.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System.Collections.Generic;
+
+namespace Services.Interfaces
+{
+	public interface ICategoriaService
+	{
+		public List<Categoria> GetAllCategorias();
+
+		public Categoria GetCategoriaById(int id);
+
+		public void AddCategoria(Categoria categoria);
+
+		public void DeleteCategoria(Categoria categoria);
+
+		public void UpdateCategoria(Categoria categoria);
+	}
+}
